50a8721c9ef75cb2d36608863e9ca1291dca46bf
Tose4591 <52237289+Tose4591@users.noreply.github.com>
2024-02-12 10:26:51 +0000

    fixing stripe error

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -1,29 +1,30 @@
-"use server";
+"use server"
 
-import Stripe from "stripe";
-
-import { CheckoutOrderParams, CreateOrderParams } from "@/types";
-import { redirect } from "next/navigation";
-import { handleError } from "../utils";
-import { connectToDatabase } from "../database";
-import Order from "../database/models/order.model";
+import Stripe from 'stripe';
+import { CheckoutOrderParams, CreateOrderParams, GetOrdersByEventParams, GetOrdersByUserParams } from "@/types"
+import { redirect } from 'next/navigation';
+import { handleError } from '../utils';
+import { connectToDatabase } from '../database';
+import Order from '../database/models/order.model';
+import Event from '../database/models/event.model';
+import { ObjectId } from 'mongodb';
+import User from '../database/models/user.model';
 
 export const checkoutOrder = async (order: CheckoutOrderParams) => {
-    const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY!);
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
     const price = order.isFree ? 0 : Number(order.price) * 100;
 
     try {
-
         const session = await stripe.checkout.sessions.create({
             line_items: [
                 {
                     price_data: {
-                        currency: "gbp",
+                        currency: 'usd',
                         unit_amount: price,
                         product_data: {
                             name: order.eventTitle
-                        },
+                        }
                     },
                     quantity: 1
                 },
@@ -37,7 +38,7 @@ export const checkoutOrder = async (order: CheckoutOrderParams) => {
             cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
         });
 
-        redirect(session.url);
+        redirect(session.url!)
     } catch (error) {
         throw error;
     }
@@ -54,7 +55,6 @@ export const createOrder = async (order: CreateOrderParams) => {
         });
 
         return JSON.parse(JSON.stringify(newOrder));
-
     } catch (error) {
         handleError(error);
     }
